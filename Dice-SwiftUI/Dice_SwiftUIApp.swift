1e7283a5b34d095c2508b3741b14ef0b0f975851
hinn254 <bennyhinnotieno@gmail.com>
2021-06-12 22:07:35 +0300

    Initial Commit

---
+++ Dice-SwiftUI/Dice_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_SwiftUIApp.swift
+//  Dice-SwiftUI
+//
+//  Created by Benny Hinn on 12/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct Dice_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
